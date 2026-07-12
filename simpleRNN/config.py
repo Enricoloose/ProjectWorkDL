c703981c726dcef3e7d29924d68f79285d5ec0d1
Enricoloose <104847152+Enricoloose@users.noreply.github.com>
2026-07-12 10:08:00 +0200

    Create config.py for dataset directory validation
    Add configuration file to check dataset directories

---
+++ simpleRNN/config.py
@@ -0,0 +1,18 @@
+import os
+
+def checkDir(directory):
+    if not(os.path.exists(directory)):
+        print("Directory non trovata: ",directory)
+
+#PATH per il dataset
+
+CURRENT_DIR = os.getcwd()
+
+DATA_DIR = os.path.join(CURRENT_DIR,'data','esercizi')
+checkDir(DATA_DIR)
+
+TRAIN_DIR = os.path.join(DATA_DIR,'train')
+checkDir(TRAIN_DIR)
+
+VALIDATION_DIR = os.path.join(DATA_DIR,'validation')
+checkDir(VALIDATION_DIR)
